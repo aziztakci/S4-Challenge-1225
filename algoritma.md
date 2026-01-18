a6375217a09898cd015e4812cde984a0f43eb0d9
aziztakci <takciazizz@gmail.com>
2026-01-18 14:24:41 +0300

    3. soru çözümü

--- algoritma.md
+++ algoritma.md
@@ -1,24 +1,29 @@
-`siparisiIsle adında high order fonksiyon yaz.`
+``tüm siparişleri kontrol dolaşıp, hangi üründen kaç tane kullanıldığını tespit eden fonksiyonu yaz.``
 
-`siparişler içindeki her objenin urunler dizisini tara.`
-`örn. 5 numaralı masa için.`
+``öncelikle her siparişte ürünid ve ürün adeti veren bir değişken tutulmalı,``
+``fonksiyonu şu şekilde olabilir,``
 
-[{
-  "urunId": 1,
-  "adet": 2
- }, 
- {
-  "urunId": 4,
-  "adet": 2
- },
- {
-  "urunId": 7,
-  "adet": 1
- }]
-  böyle bir dizi çıkaracak.
+(flatmap ve map ile)
 
-`urunId karşılığındaki ürünü menuler.urunler içindeki objelerin her birinde ara.`
+``siparisUrunleri = urunId adet ve sipariş saatini içermekte.``
 
+önce verilen sipariş sonrası malzemeleri toplamak.
 
- 
-`bulduğunda id numarasını içeren o objenin istenen özelliklerini callback ile al,`
+sort metoduyla saat bilgilerini kıyaslayabiliriz,
+
+`siraliSiparis ile önceliğie göre sıraladık.`
+
+[
+  { urunId: 1, adet: 2, saat: "12:30" },
+  { urunId: 4, adet: 2, saat: "12:30" },
+  { urunId: 7, adet: 1, saat: "12:30" },
+  { urunId: 2, adet: 1, saat: "13:00" },
+  { urunId: 5, adet: 3, saat: "13:00" },
+  { urunId: 8, adet: 2, saat: "13:00" },
+  { urunId: 3, adet: 2, saat: "13:15" },
+  { urunId: 6, adet: 2, saat: "13:15" },
+  { urunId: 7, adet: 2, saat: "13:15" }
+]  çıktı bu şekilde.
+
+`her birini kontrol edip menüler içinde hangi ürün olduğunu ve hangi malzemeden kaç tane kullanıldığına bakılacak. flatmap ile restoran.menuler düzleştirilip, içinde find ile yeni listeyi bir değişkene atayıp onun üzerinde çalışılabilir`
+
--- cozum.js
+++ cozum.js
@@ -33,14 +33,13 @@ const menudeAra = (clb) => {
   return liste.filter(clb);
 };
 
-
 /* 2. Soru Çözümü */
 
 let siparisDetay = (urun, adet) => {
   return {
     ad: urun.ad,
     adet: adet,
-    toplam: urun.fiyat * adet
+    toplam: urun.fiyat * adet,
   };
 };
 
@@ -48,22 +47,18 @@ const siparisiIsle = (masaNo, clb) => {
   let sonuc = [];
   let result = [];
 
-  restoran.siparisler.forEach(siparis => {
+  restoran.siparisler.forEach((siparis) => {
     if (siparis.masa === masaNo) {
       sonuc.push(...siparis.urunler);
     }
   });
 
-  restoran.menuler.forEach(kategori => {
-    kategori.urunler.forEach(urun => {
-      const eslesme = sonuc.find(
-        obje => urun.id === obje.urunId
-      );
+  restoran.menuler.forEach((kategori) => {
+    kategori.urunler.forEach((urun) => {
+      const eslesme = sonuc.find((obje) => urun.id === obje.urunId);
 
       if (eslesme) {
-        result.push(
-          clb(urun, eslesme.adet)
-        );
+        result.push(clb(urun, eslesme.adet));
       }
     });
   });
@@ -73,4 +68,36 @@ const siparisiIsle = (masaNo, clb) => {
 
 console.log(siparisiIsle(5, siparisDetay));
 
+/* 3. Soru Çözümü */
 
+const toplamStok = {...restoran.stok};
+
+let siparisUrunleri = restoran.siparisler.flatMap((order) => {
+  return order.urunler.map((orderTime) => ({
+    urunId: orderTime.urunId,
+    adet: orderTime.adet,
+    saat: order.saat,
+  }));
+});
+
+let siraliSiparis = siparisUrunleri.sort((a, b) => {
+  let [aSaat, aDakika] = a.saat.split(":").map(Number);
+  let [bSaat, bDakika] = b.saat.split(":").map(Number);
+  return aSaat !== bSaat ? aSaat - bSaat : aDakika - bDakika;
+});
+
+siraliSiparis.forEach((urun) => {
+  const siparisDetay = restoran.menuler
+    .flatMap((u) => u.urunler)
+    .find((s) => s.id === urun.urunId);
+
+    siparisDetay.malzemeler.forEach(malzeme => {
+      if (restoran.stok[malzeme] >= urun.adet) {
+        restoran.stok[malzeme] -= urun.adet;
+        let stokYuzde = (restoran.stok[malzeme] / toplamStok[malzeme]);
+        console.log(`${urun.adet} adet ${malzeme} kullanıldı. Kalan: ${restoran.stok[malzeme]} adet( %${(stokYuzde*100).toFixed(2)}). `)
+      } else {
+        console.log(`Stoklarda ${malzeme} yetersiz. Stok ${restoran.stok[malzeme]}.`)
+      }
+    })
+});
